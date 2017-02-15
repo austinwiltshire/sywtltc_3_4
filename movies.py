80156ced68605e0bf843b988bf07e0c04b928e59
emdavis7 <ericd@simpli.fi>
2017-02-15 17:16:48 -0600

    Add enough_money function
    Changed function to make cash an integer.

--- movies.py
+++ movies.py
@@ -4,15 +4,32 @@ MOVIE_PRICE = 5.0
 
 def enough_money(customer):
     """Check if customer has adequate funds"""
-    if customer["cash"] < 5.0:
+    balance = customer["cash"]
+    if balance < 5.0:
         return False
     return True
 
-def purchase(movie, customer):
-    """Deduct $5 from moviegoer's account"""
-    customer["cash"] = customer["cash"] - 5.0
-#   customer["movies"] = customer["movies"].append(movie)
+def purchase_ticket(movie, customer):
+    """Performs the steps required in purchasing a ticket and returns altered customer and movie"""
+    customer = charge(customer)
+    customer = dispense_ticket(movie, customer)
+    movie = remove_seat(movie)
+    return customer, movie
+
+def charge(customer):
+    """Charge customer $5"""
+    balance = customer["cash"]
+    new_balance = balance - 5.0
+    customer["cash"] = new_balance
     return customer
+def dispense_ticket(movie, customer):
+    """Add movie to customer's account"""
+    customer["movies"] = customer["movies"].append(movie)
+    return customer
+def remove_seat(movie):
+    """Remove seat from ticket database"""
+    movie["seats"] = movie["seats"] - 1
+    return movie
 
 def refund(movie, customer):
     """Add $5 back to moviegoer's account"""
--- movies_test.py
+++ movies_test.py
@@ -13,34 +13,19 @@ CUSTOMERS = {
               "cash" : 4.0}
 }
 MOVIE_TICKETS = {
-    "Xmen 8: The Xmennening": 10,
-    "The Bromance" : 20,
-    "Gigli: The Play: The Book: The movie" : 102
+    "Xmen 8: The Xmennening" : {"seats" : 10},
+    "The Bromance" : {"seats" : 20},
+    "Gigli: The Play: The Book: The movie" : {"seats" : 102}
 }
 
 def test_enough_money():
-    """Tests the purchase ticket function"""
+    """Tests the enough money function"""
     assert movies.enough_money("Ricci") is False
     assert movies.enough_money("Bob") is True
 
-def test_purchase():
-    """Tests $5 charge to account"""
-    customer = movies.purchase({"movies": [], "cash": 100.0})
-    actual_cash = customer["cash"]
+def test_charge():
+    """Tests charge function"""
+    bob = movies.charge("Bob")
+    actual_cash = bob["cash"]
     expected_cash = 95.0
-    assert actual_cash == expected_cash
-    customer = movies.purchase({"movies": [], "cash": 7.0})
-    actual_cash = customer["cash"]
-    expected_cash = 2.0
-    assert actual_cash == expected_cash
-
-def test_refund():
-    """Tests $5 refund to account"""
-    customer = movies.refund({"movies": [], "cash": 50.0})
-    actual_cash = customer["cash"]
-    expected_cash = 55.0
-    assert actual_cash == expected_cash
-    customer = movies.refund({"movies": [], "cash": 15.0})
-    actual_cash = customer["cash"]
-    expected_cash = 20.0
     assert actual_cash == expected_cash
