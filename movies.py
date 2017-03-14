2000cb29333da1db5a9494ddcef658de1f5b6909
emdavis7 <ericd@simpli.fi>
2017-03-13 23:12:39 -0500

    Add print all of library functions
    Used pretty print to return all raw data.

--- movies.py
+++ movies.py
@@ -2,6 +2,29 @@
 
 MOVIE_PRICE = 5.0
 
+import pprint
+    
+def customer_db():
+    """Define Customer database for testing"""
+    return {
+        "Bob" : {"movies":[],
+                 "cash" : 100.0},
+        "Jim" : {"movies": ["Xmen 8: The Xmennening"],
+                 "cash" : 10.0},
+        "Cary" : {"movies": ["Gigli: The Play: The Book: The movie", "The Bromance"],
+                  "cash" : 120.0},
+        "Ricci": {"movies": [],
+                  "cash" : 4.0}
+    }
+
+def movie_db():
+    """Define movie database for testing"""
+    return {
+        "Xmen" : {"title" : "Xmen 8: The Xmennening", "seats_avaliable" : 10},
+        "Bromance" : {"title" : "The Bromance", "seats_avaliable" : 20},
+        "Gigli" : {"title" : "Gigli: The Play: The Book: The movie", "seats_avaliable" : 102}
+    }
+
 def enough_money(customer):
     """Check if customer has adequate funds"""
     balance = customer["cash"]
@@ -63,10 +86,16 @@ def print_movie_data(movie):
 
 def print_customer_data(customer): #movies have brackets around them
     """Prints customer database information"""
-    movie_list = customer["movies"]
-    print("Movie Tickets:         ", movie_list)
+    movie_list = customer_db()
+    print("Movie Tickets:         ", customer["movies"])
     print("Account Balance:       ", customer["cash"])
 
-# def print_all_movie_data(movie_db()):
+def print_all_movie_data():
+    """Print all movie data raw"""
+    __movies__ = movie_db()
+    pprint.pprint(__movies__)
 
-# def print_all_customer_data(customer_db()):
+def print_all_customer_data():
+    """Print all customer data raw"""
+    __customers__ = customer_db()
+    pprint.pprint(__customers__)
