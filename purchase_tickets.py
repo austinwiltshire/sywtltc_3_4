ad35464fd83a4986878224176fdf2836d2c5d174
emdavis7 <ericd@simpli.fi>
2017-03-23 15:55:17 -0500

    Fix docstrings for functions
    Changed docstrings to be more explicit in adding Args and Returns.

--- purchase_tickets.py
+++ purchase_tickets.py
@@ -3,8 +3,19 @@
 import purchase_ticket_details
 
 def purchase_ticket(movie, customer):
-    """Performs the steps required in purchasing a ticket and returns altered customer and movie"""
-    assert purchase_ticket_details.enough_money(customer)
+    """Performs the steps required in purchasing a ticket and returns altered customer and movie
+
+    Args:
+        movie (dict): dictionary containing 'title' and 'seats_available' for each movie.
+        customer (dict): dictionary containing 'movies' and 'cash' keys for each customer.
+
+    Returns:
+        movie: (dict) dictionary containing 'title' unchanged, and 'seats_remaining'
+        reduced by 1.
+        customer: (dict) dictionary containing appended movie title to 'movies' key.
+        'cash' key reduced by MOVIE_PRICE.
+    """
+    assert purchase_ticket_details.enough_money(customer["cash"])
     customer = purchase_ticket_details.dispense_ticket(movie,
                                                        purchase_ticket_details.charge(customer))
     movie = purchase_ticket_details.remove_seat(movie)
--- refund_tickets.py
+++ refund_tickets.py
@@ -3,7 +3,17 @@
 import refund_tickets_details
 
 def refund_ticket(movie, customer):
-    """Performs the steps required in refunding a ticket and returns altered customer and movie"""
+    """Performs the steps required in refunding a ticket and returns altered customer and movie
+    
+    Args:
+        movie (dict): dictionary containing 'title' and 'seats_available' for each movie.
+        customer (dict): dictionary containing 'movies' and 'cash' keys for each customer.
+
+    Returns:
+        movie: (dict) dictionary containing 'title' unchanged, and 'seats_remaining'
+        increased by 1.
+        customer: (dict) dictionary containing removed movie title in 'movies' key.
+        'cash' key increased by MOVIE_PRICE."""
     customer = refund_tickets_details.remove_ticket(movie,
                                                     refund_tickets_details.add_funds(customer))
     movie = refund_tickets_details.add_seat(movie)
